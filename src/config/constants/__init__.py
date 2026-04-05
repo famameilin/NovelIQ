8794fd9f7f5d2a35489ad589d6a9fd21df929fdb
32891 <3289195457@qq.com>
2026-04-05 12:13:12 +0800

    feat(graph): 对称关系自动生成反向边，添加关系类型验证常量

--- src/config/constants/__init__.py
+++ src/config/constants/__init__.py
@@ -15,7 +15,10 @@ from src.config.constants.annotation import (
     EMOTION_SCORE_MAPPING,
     PHASE3_MAX_RETRIES,
     PHASE_MAX_RETRIES,
+    SYMMETRIC_RELATION_TYPES,
     VALIDATION_MAX_RETRIES,
+    VALID_CHANGE_TYPES,
+    VALID_RELATION_TYPES,
 )
 from src.config.constants.disambiguation import MAX_DISAMBIG_RETRIES
 from src.config.constants.text_normalization import (
@@ -91,4 +94,7 @@ __all__ = [
     "PHASE3_MAX_RETRIES",
     "VALIDATION_MAX_RETRIES",
     "MAX_DISAMBIG_RETRIES",
+    "SYMMETRIC_RELATION_TYPES",
+    "VALID_RELATION_TYPES",
+    "VALID_CHANGE_TYPES",
 ]
